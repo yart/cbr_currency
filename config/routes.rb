8f72828c4ca8d5ba34528e1f15dc3ae323f38ae0
yart <jarocki.art@gmail.com>
2023-11-26 21:23:22 +0300

    cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # get 'currency_rates/index'
   root 'currency_rates#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match '*path', to: 'application#render_404', via: :all
 end
