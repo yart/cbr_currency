7037221a368c17631c676e38e89c95d68dc69562
yart <jarocki.art@gmail.com>
2023-11-26 21:25:59 +0300

    refactored tests

--- spec/models/currency_rate_spec.rb
+++ spec/models/currency_rate_spec.rb
@@ -1,35 +1,38 @@
 require 'rails_helper'
 
-# RSpec.describe CurrencyRate, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
 RSpec.describe CurrencyRate, type: :model do
+  let(:usd)       { 'USD' }
+  let(:eur)       { 'EUR' }
+  let(:today)     { Date.today }
+  let(:yesterday) { Date.yesterday }
+  let(:usd_rate)  { CurrencyRate.new(date: today, currency_code: usd, exchange_rate: 1.0) }
+
   it 'should not save currency rate without date, currency code, and exchange rate' do
     currency_rate = CurrencyRate.new
     expect(currency_rate.save).to be_falsey
   end
 
   it 'should save valid currency rate' do
-    currency_rate = CurrencyRate.new(date: Date.today, currency_code: 'USD', exchange_rate: 1.0)
+    currency_rate = usd_rate
     expect(currency_rate.save).to be_truthy
   end
 
   it 'should find currency rate by date and currency code' do
-    currency_rate = CurrencyRate.new(date: Date.today, currency_code: 'USD', exchange_rate: 1.0)
+    currency_rate = usd_rate
     currency_rate.save
 
-    found_rate = CurrencyRate.find_by(date: Date.today, currency_code: 'USD')
+    found_rate = CurrencyRate.find_by(date: today, currency_code: usd)
     expect(found_rate).to eq(currency_rate)
   end
 
   it 'should not find currency rate with incorrect date or currency code' do
-    currency_rate = CurrencyRate.new(date: Date.today, currency_code: 'USD', exchange_rate: 1.0)
+    currency_rate = usd_rate
     currency_rate.save
 
-    incorrect_date_rate = CurrencyRate.find_by(date: Date.yesterday, currency_code: 'USD')
-    incorrect_currency_rate = CurrencyRate.find_by(date: Date.today, currency_code: 'EUR')
+    incorrect_date_rate = CurrencyRate.find_by(date: yesterday, currency_code: usd)
+    incorrect_currency_rate = CurrencyRate.find_by(date: today, currency_code: eur)
 
-    expect(incorrect_date_rate).to be_nil
+    expect(incorrect_date_rate).to     be_nil
     expect(incorrect_currency_rate).to be_nil
   end
 
@@ -37,7 +40,7 @@ RSpec.describe CurrencyRate, type: :model do
     currency_rate = CurrencyRate.new
 
     expect(currency_rate).to_not be_valid
-    expect(currency_rate.errors[:date]).to include("can't be blank")
+    expect(currency_rate.errors[:date]).to          include("can't be blank")
     expect(currency_rate.errors[:currency_code]).to include("can't be blank")
     expect(currency_rate.errors[:exchange_rate]).to include("can't be blank")
   end
--- spec/services/currency_rate_updater_spec.rb
+++ spec/services/currency_rate_updater_spec.rb
@@ -1,66 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe CurrencyRateUpdater, type: :service, vcr: true do
+  let(:usd) { 'USD' }
+  let(:eur) { 'EUR' }
+  let(:some_date)  { Date.new(2023, 2, 8) }
+  let(:other_date) { Date.new(2023, 2, 9) }
+  let(:url) { "https://www.cbr.ru/scripts/XML_daily.asp?date_req=#{some_date.strftime('%d/%m/%Y')}" }
+
+  describe '.form_url' do
+    it 'forms the correct URL' do
+      expected_url = url
+      actual_url   = CurrencyRateUpdater.form_url(some_date)
+
+      expect(actual_url).to eq(expected_url)
+    end
+  end
+
+  describe '.fetch_xml_data' do
+    it 'handles unsuccessful HTTP response' do
+      allow(Net::HTTP).to receive(:get_response).and_return(Net::HTTPNotFound.new('1.1', '404', 'Not Found'))
+
+      result = CurrencyRateUpdater.fetch_xml_data(url)
+
+      expect(result).to be_nil
+    end
+  end
+
   describe '.update_for_date' do
     it 'should update currency rates for the given date' do
       VCR.use_cassette('currency_rates') do
-        date = Date.new(2023, 2, 9)
-        CurrencyRateUpdater.update_for_date(date)
+        CurrencyRateUpdater.update_for_date(other_date)
 
-        usd_rate = CurrencyRate.find_by_date_and_currency_code(date, 'USD')
-        eur_rate = CurrencyRate.find_by_date_and_currency_code(date, 'EUR')
+        usd_rate = CurrencyRate.find_by_date_and_currency_code(other_date, usd)
+        eur_rate = CurrencyRate.find_by_date_and_currency_code(other_date, eur)
 
         expect(usd_rate).not_to be_nil
         expect(eur_rate).not_to be_nil
       end
     end
 
-    it 'handles unsuccessful HTTP response' do
-      date = Date.new(2023, 2, 10)
-      url = "https://www.cbr.ru/scripts/XML_daily.asp?date_req=#{date.strftime('%d/%m/%Y')}"
-
-      allow(Net::HTTP).to receive(:get_response).and_return(Net::HTTPNotFound.new('1.1', '404', 'Not Found'))
-
-      expect(Rails.logger).to receive(:error).with("Failed to fetch data from #{url}: HTTP error 404")
-
-      CurrencyRateUpdater.update_for_date(date)
-    end
-
     it 'handles XML parsing errors' do
-      date = Date.new(2023, 2, 10)
-      url = "https://www.cbr.ru/scripts/XML_daily.asp?date_req=#{date.strftime('%d/%m/%Y')}"
-
       allow(CurrencyRateUpdater).to receive(:fetch_xml_data) { '<invalid_xml>' }
-      CurrencyRateUpdater.update_for_date(date)
 
-      expect(CurrencyRateUpdater.update_for_date(date)).to be_nil
-    end
+      result = CurrencyRateUpdater.update_for_date(some_date)
 
-    it 'forms the correct URL' do
-      date = Date.new(2023, 2, 10)
-      expected_url = "https://www.cbr.ru/scripts/XML_daily.asp?date_req=#{date.strftime('%d/%m/%Y')}"
-
-      allow(Rails.logger).to receive(:info)
-      actual_url = CurrencyRateUpdater.form_url(date)
-
-      expect(actual_url).to eq(expected_url)
+      expect(result).to be_nil
     end
 
     it 'updates the record only if the exchange rate has changed' do
-      date = Date.new(2023, 2, 10)
-      currency_code = 'USD'
+      currency_code = usd
       existing_exchange_rate = 71.5763
       new_exchange_rate = 72.1234
 
       existing_record = create(
         :currency_rate,
-        date: date,
+        date: some_date,
         currency_code: currency_code,
         exchange_rate: existing_exchange_rate
       )
 
       allow(CurrencyRate).to receive(:find_or_initialize_by).and_return(existing_record)
-      CurrencyRateUpdater.update_or_create_currency_rate(date, currency_code, new_exchange_rate)
+
+      CurrencyRateUpdater.update_or_create_currency_rate(some_date, currency_code, new_exchange_rate)
+
       expect(existing_record.reload.exchange_rate).to eq(new_exchange_rate)
     end
   end
