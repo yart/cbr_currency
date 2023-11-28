c7002dc524f156a82eb95b50e883e2bba3e24f5e
yart <jarocki.art@gmail.com>
2023-11-28 14:15:48 +0300

    fixed a bug with table: rate changes weren't calculated correct

--- app/controllers/currency_table_controller.rb
+++ app/controllers/currency_table_controller.rb
@@ -1,38 +1,33 @@
 class CurrencyTableController < ApplicationController
   def index
-    week_labels     = ['валюта', '4 недели назад', '3 недели назад', 'позапрошлая неделя', 'прошлая неделя']
-    @currency_rates = CurrencyRate.where('date >= ?', start_date).order(:date)
+    # Looks like better is to move these literals to i18n…
+    @week_labels    = ['валюта', '4 недели назад', '3 недели назад', 'позапрошлая неделя', 'прошлая неделя']
+    @start_date     = Date.current.beginning_of_week - 4.weeks
+    @currency_rates = CurrencyRate.where('date >= ?', @start_date).order(:date)
     @grouped_rates  = @currency_rates.group_by(&:currency_code)
-    @table_data     = build_currency_table_data(week_labels)
+    @table_data     = build_currency_table_data
   end
 
   private
 
-  def start_date
-    today      = Date.current
-    start_date = today.beginning_of_week
-
-    start_date += 1.day if today.saturday? || today.sunday?
-
-    (start_date - 28.days).beginning_of_week
-  end
-
-  def build_currency_table_data(week_labels)
-    table_data = { header: week_labels, currencies: [] }
+  def build_currency_table_data
+    table_data = { header: @week_labels, currencies: [] }
 
     @grouped_rates.each do |currency_code, rates|
       week_data = [currency_code]
 
-      week_labels[1..-1].each_with_index do |_, index|
-        monday_of_week = (index + 1).weeks.ago.to_date.beginning_of_week
-        rate_on_monday = rates.find { |rate| rate.date == monday_of_week }
+      # To skip the currency column. Maybe better to use 4.times here, but I hope the current way is 
+      # more clearly to understand what is happening here.
+      @week_labels[1..-1].each_with_index do |_, index|
+        monday_of_week = @start_date.since(index.weeks)
+        rate_on_monday = find_rate(rates, monday_of_week)
 
         unless rate_on_monday
-          week_data << { rate: 'N/A', growth: 'N/A' }
+          week_data << 'N/A'
           next
         end
 
-        week_data << calculate_rate_and_growth(rate_on_monday, rates, index)
+        week_data << calculate_rate_and_growth(rates, rate_on_monday, monday_of_week)
       end
 
       table_data[:currencies] << week_data
@@ -41,13 +36,22 @@ class CurrencyTableController < ApplicationController
     table_data
   end
 
-  def calculate_rate_and_growth(rate_on_monday, rates, week_index)
-    friday_of_week = (week_index + 1).weeks.ago.to_date.end_of_week
-    rate_on_friday = rates.find { |rate| rate.date == friday_of_week }
+  def calculate_rate_and_growth(rates, rate_on_monday, date_on_monday)
+    # ATTENTION: CBR doesn't change any currency rate during weekend, so we can use friday as the end of any week
+    friday_of_week = date_on_monday + 4.days
+    rate_on_friday = find_rate(rates, friday_of_week)
 
     return 'N/A' unless rate_on_friday
 
-    growth_percentage = ((rate_on_friday.exchange_rate - rate_on_monday.exchange_rate) / rate_on_monday.exchange_rate * 100).round(2)
-    "#{rate_on_friday.exchange_rate.round(2)} ₽ (#{growth_percentage.positive? ? '+' + growth_percentage.to_s : growth_percentage}%)"
+    mondays_rate      = rate_on_monday.exchange_rate
+    fridays_rate      = rate_on_friday.exchange_rate
+    growth_percentage = ((fridays_rate - mondays_rate) / mondays_rate * 100).round(2)
+    formatted_growth  = (growth_percentage.positive? ? '+' + growth_percentage.to_s : growth_percentage.to_s) + '%'
+
+    "#{rate_on_monday.exchange_rate.round(2)} ₽ (#{formatted_growth})"
+  end
+
+  def find_rate(rates, day)
+    rates.find { |rate| rate.date == day }
   end
 end
